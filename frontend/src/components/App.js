f2e5cb2528ea72241e7e97ec461e67f5729a9914
elicrock <rockelic@yandex.ru>
2023-08-01 23:13:06 +0300

    Sprint 15 - some fix

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -130,7 +130,7 @@ function App() {
   }
 
   function handleCardLike(card) {
-    const isLiked = card.likes.some(i => i._id !== currentUser._id);
+    const isLiked = card.likes.some(i => i === currentUser._id);
 
     async function makeRequest() {
       const newCard = await api.changeLikeCardStatus(card._id, isLiked);
