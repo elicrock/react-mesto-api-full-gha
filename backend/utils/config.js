6caa2815a3b509b4d33d84eef5737564f3a665dd
elicrock <rockelic@yandex.ru>
2023-08-03 15:10:06 +0300

    Sprint 15 - some fix

--- backend/utils/config.js
+++ backend/utils/config.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const PORT = process.env.PORT || 3000;
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/mestodb';
 const JWT_SECRET = process.env.PROD === 'production' ? process.env.JWT_SECRET : 'dev-secret';
+console.log(JWT_SECRET);
 
 const mongooseOptions = {
   useNewUrlParser: true,
